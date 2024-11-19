0d57da1deefc63d139ac8b75f3f7f663fe7b085e
Michał Nowicki <michal5520pl@gmail.com>
2024-11-19 21:11:04 +0000

    Library fixes

--- src/compressor.go
+++ src/compressor.go
@@ -1,60 +1,61 @@
 package main
 
 import (
-    "image"
-    "image/jpeg"
-    "image/png"
-    "image/bmp"
-    "os"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+
+	"golang.org/x/image/bmp"
 )
 
-
-//funkcja przyjmuje ścieżkę do pliku wejściowego i ścieżke do pliku zwracanego
-//przyjmuje formatach PNG i BMP - zapisuje w formacie JPEG
+// funkcja przyjmuje ścieżkę do pliku wejściowego i ścieżke do pliku zwracanego
+// przyjmuje formatach PNG i BMP - zapisuje w formacie JPEG
 func CompressImage(inputPath string, outputPath string, quality int) error {
 
-    // przyjmuje plik wejściowy
-    inputFile, err := os.Open(inputPath)
-    if err != nil {
-        return err
-    }
-    defer inputFile.Close()
+	// przyjmuje plik wejściowy
+	inputFile, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
 
-    //dekoduje obraz
-    var img image.Image
-    switch ext := getFileExtension(inputPath); ext {
-    case ".png":
-        img, err = png.Decode(inputFile)
-    case ".bmp":
-        img, err = bmp.Decode(inputFile)
-    default:
-        return fmt.Errorf("unsupported file format: %s", ext)
-    }
+	//dekoduje obraz
+	var img image.Image
+	switch ext := getFileExtension(inputPath); ext {
+	case ".png":
+		img, err = png.Decode(inputFile)
+	case ".bmp":
+		img, err = bmp.Decode(inputFile)
+	default:
+		return fmt.Errorf("unsupported file format: %s", ext)
+	}
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    //inicjuje plik wyjściowy
-    outputFile, err := os.Create(outputPath)
-    if err != nil {
-        return err
-    }
-    defer outputFile.Close()
+	//inicjuje plik wyjściowy
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
 
-    //ustawienia kompresji dla formatu JPEG
-    jpegOptions := jpeg.Options{Quality: quality}
+	//ustawienia kompresji dla formatu JPEG
+	jpegOptions := jpeg.Options{Quality: quality}
 
-    //zapisuje obraz w JPEG
-    err = jpeg.Encode(outputFile, img, &jpegOptions)
+	//zapisuje obraz w JPEG
+	err = jpeg.Encode(outputFile, img, &jpegOptions)
 
-    return err
+	return err
 }
 
-//funkcja pobiera rozszenie pliku
+// funkcja pobiera rozszenie pliku
 func getFileExtension(filename string) string {
-    if len(filename) < 4 {
-        return ""
-    }
-    return filename[len(filename)-4:]
+	if len(filename) < 4 {
+		return ""
+	}
+	return filename[len(filename)-4:]
 }
--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,7 @@ module github.com/Lolillkillu/image_compression_wszib
 
 go 1.23.0
 
-require github.com/tomcraven/goga v0.0.0-20220413070930-f4ca47f4d421 // indirect
+require (
+	github.com/tomcraven/goga v0.0.0-20220413070930-f4ca47f4d421
+	golang.org/x/image v0.22.0
+)
